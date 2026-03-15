c1231a495e87f721f10d099a7b1ab1e10e3b21cf
nd-huwng05 <nguyendinhhung290605@gmai.com>
2026-03-15 23:18:12 +0700

    update auth

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,13 @@
 from flask import Flask
+from flask_jwt_extended import JWTManager
+
 # from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__, template_folder='templates', static_folder='static')
 app.config.from_object('config')
 
 # db = SQLAlchemy(app)
+jwt = JWTManager(app)
 
 from .api import api
 from .routes import routes
--- config.py
+++ config.py
@@ -1,1 +1,5 @@
-DEBUG = False
+from datetime import timedelta
+
+JWT_SECRET_KEY = 'CINEMAFLOW'
+JWT_ACCESS_TOKEN_EXPIRES = timedelta(days=7)
+JWT_REFRESH_TOKEN_EXPIRES = timedelta(days=30)
