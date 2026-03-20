1af81de9aa495f86c37804bd0faf799b19a4b589
dungphamkhac <2351050023dung@ou.edu.vn>
2026-03-20 15:34:49 +0700

    solved bugs and added poster attribute

--- app/models/base.py
+++ app/models/base.py
@@ -1,7 +1,9 @@
+from alembic.autogenerate.compare import server_defaults
+
 from app import db
 from sqlalchemy import func
 
 class BaseModel(db.Model):
     __abstract__ = True
-    created_at = db.Column(db.DateTime, serverdefault=func.now())
-    updated_at = db.Column(db.DateTime, serverdefault=func.now(), serveronupdate=func.now())
+    created_at = db.Column(db.DateTime, server_default=func.now())
+    updated_at = db.Column(db.DateTime, server_default=func.now(), onupdate=func.now())
