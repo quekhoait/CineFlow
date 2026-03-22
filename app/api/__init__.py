437a0b043e6e1598dfa3765ef191efaae4cdd332
nd-huwng05 <nguyendinhhung290605@gmai.com>
2026-03-22 11:06:39 +0700

    init_ticket

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -3,3 +3,6 @@ api = Blueprint('api', __name__, url_prefix='/api')
 
 from .user_api import user_api
 api.register_blueprint(user_api)
+
+from .ticket_api import ticket_api
+
---
+++ app/api/ticket_api.py
@@ -0,0 +1,3 @@
+from flask import Blueprint
+
+ticket_api = Blueprint('ticket', __name__, url_prefix='/ticket')
