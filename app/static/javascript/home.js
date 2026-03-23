f2b0636ebe7df0a17dfe832aa0241eb881972f61
quekhoait <166801979+quekhoait@users.noreply.github.com>
2026-03-23 11:08:56 +0700

    update module_film

--- app/static/javascript/home.js
+++
@@ -1,9 +0,0 @@
-function scrollSlider(sliderId, distance) {
-        const slider = document.getElementById(sliderId);
-        if (slider) {
-            slider.scrollBy({
-                left: distance,
-                behavior: 'smooth'
-            });
-        }
-    }
