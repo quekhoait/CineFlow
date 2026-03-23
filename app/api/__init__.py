d8a3ebfcbefcc29a2f225cb052c2835b510afed4
quekhoait <166801979+quekhoait@users.noreply.github.com>
2026-03-23 11:12:11 +0700

    update interface

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -2,9 +2,7 @@ from flask import Blueprint
 api = Blueprint('api', __name__, url_prefix='/api')
 
 from .user_api import user_api
-from .film_api import film_api
-from .cinema_api import cinema_api
+
 
 api.register_blueprint(user_api)
-api.register_blueprint(film_api)
-api.register_blueprint(cinema_api)
+
