e2976aa61eb3b0470d8a621e7160ddf18d76fccf
nd-huwng05 <nguyendinhhung290605@gmai.com>
2026-05-03 11:35:59 +0700

    update

--- app/pattern/notification.py
+++ app/pattern/notification.py
@@ -1,6 +1,9 @@
 from abc import ABC, abstractmethod
-from flask_mail import Message
 from typing import Optional, List, Dict, Any
+
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail, Attachment, FileContent, FileName, FileType, Disposition
+from flask import current_app
 
 from app.utils.errors import SendEmailFailed
 
@@ -8,30 +11,42 @@ from app.utils.errors import SendEmailFailed
 class NotificationSender(ABC):
     @abstractmethod
     def send(self, recipient: str, subject: str, body: str, attachments: Optional[List[Dict[str, Any]]] = None) -> bool:
-        pass # pragma: no cover
+        pass
+
 
 class EmailSender(NotificationSender):
-    def __init__(self, mail_client):
-        self.mail_client = mail_client
+    def __init__(self):
+        self.api_key = current_app.config["SENDGRID_API_KEY"]
+        self.from_email = current_app.config["SENDGRID_FROM"]
 
     def send(self, recipient: str, subject: str, body: str, attachments: Optional[List[Dict[str, Any]]] = None) -> bool:
-        msg = Message(
-            subject=subject or "Notification from CineFlow",
-            recipients=[recipient],
-        )
-
-        if body:
-            msg.body = body
-
-        if attachments:
-            for file in attachments:
-                msg.attach(
-                    filename=file["filename"],
-                    content_type=file["content_type"],
-                    data=file["data"],
-                )
         try:
-            self.mail_client.send(msg)
-            return True
-        except Exception:
-            raise SendEmailFailed()
+            message = Mail(
+                from_email=self.from_email,
+                to_emails=recipient,
+                subject=subject or "Notification from CineFlow",
+                plain_text_content=body
+            )
+
+            if attachments:
+                import base64
+                attachment_list = []
+
+                for file in attachments:
+                    encoded_file = base64.b64encode(file["data"]).decode()
+
+                    attachment = Attachment(
+                        FileContent(encoded_file),
+                        FileName(file["filename"]),
+                        FileType(file["content_type"]),
+                        Disposition("attachment")
+                    )
+                    attachment_list.append(attachment)
+
+                message.attachment = attachment_list
+
+            sg = SendGridAPIClient(self.api_key)
+            sg.send(message)
+
+        except Exception as e:
+            raise e
