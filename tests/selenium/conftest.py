def4577b289fa4c9e68fa008b7b3245c20e6751d
dungphamkhac <2351050023dung@ou.edu.vn>
2026-05-21 16:12:44 +0700

    create celenium for HistoryFE

---
+++ tests/selenium/conftest.py
@@ -0,0 +1,36 @@
+import threading
+import time
+import pytest
+from selenium import webdriver
+from app import create_app, db
+
+@pytest.fixture(scope="session")
+def app_instance():
+    app = create_app('testing')
+
+    with app.app_context():
+        db.drop_all()
+        db.create_all()
+        yield app
+        db.session.remove()
+        db.drop_all()
+
+@pytest.fixture(scope="session")
+def local_server_url(app_instance):
+    server_thread = threading.Thread(
+        target=lambda: app_instance.run(host='127.0.0.1', port=8000, use_reloader=False, debug=False)
+    )
+    server_thread.daemon = True
+    server_thread.start()
+    time.sleep(2)
+    # return "http://127.0.0.1:5000"
+    return "https://www.ndhuwng05.me/"
+
+@pytest.fixture
+def driver():
+    options = webdriver.ChromeOptions()
+    # options.add_argument('--headless')
+    driver = webdriver.Chrome(options=options)
+    driver.maximize_window()
+    yield driver
+    driver.quit()
